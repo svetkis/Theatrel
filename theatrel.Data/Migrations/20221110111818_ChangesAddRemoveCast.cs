b9caa338ab298202f3f36c790537d22d96d84508
svetkis <sdudich@yandex.ru>
2022-11-10 14:24:57 +0300

    -

---
+++ theatrel.Data/Migrations/20221110111818_ChangesAddRemoveCast.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace theatrel.DataAccess.Migrations
+{
+    public partial class ChangesAddRemoveCast : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CastAdded",
+                table: "PlaybillChanges",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CastRemoved",
+                table: "PlaybillChanges",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CastAdded",
+                table: "PlaybillChanges");
+
+            migrationBuilder.DropColumn(
+                name: "CastRemoved",
+                table: "PlaybillChanges");
+        }
+    }
+}
