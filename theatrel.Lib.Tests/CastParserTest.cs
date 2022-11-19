8dd3e5bfa818698489612bde2468998be96d0fe6
svetkis <sdudich@yandex.ru>
2022-11-19 11:17:00 +0300

    -

---
+++ theatrel.Lib.Tests/CastParserTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using theatrel.Interfaces.Cast;
+using theatrel.Lib.Enums;
+using Xunit;
+
+namespace theatrel.Lib.Tests
+{
+    public class CastParserTest
+    {
+        [Theory]
+        [InlineData("https://www.mariinsky.ru/playbill/playbill/2022/11/20/8_1600/")]
+        public async void Test(string url)
+        {
+            var factory = DIContainerHolder.Resolve<Func<Theatre, IPerformanceCastParser>>();
+            var parser = factory(Theatre.Mariinsky);
+
+            await parser.ParseFromUrl(url, false, CancellationToken.None);
+        }
+    }
+}
