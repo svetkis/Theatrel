a546a08238bcbc583b07c84e9f92e092a0085cb7
svetkis <sdudich@yandex.ru>
2022-12-06 23:23:10 +0300

    -

--- theatrel.Lib/MihailovkyParsers/MihailovskyCastParser.cs
+++ theatrel.Lib/MihailovkyParsers/MihailovskyCastParser.cs
@@ -70,9 +70,12 @@ internal class MihailovskyCastParser : IPerformanceCastParser
             if (!castBlock.Any())
             {
                 var dlBlock = parsedDoc.QuerySelectorAll("dl");
-                if (dlBlock.Any() && dlBlock.First().Children.Any())
+                if (dlBlock.Any())
                 {
-                    castBlock = new IElement[] { dlBlock.First().Children.Last() };
+                    var dtBlock = dlBlock.First().Children.FirstOrDefault();
+                    
+                    if (string.Equals(dtBlock?.TextContent, "исполнители", StringComparison.InvariantCultureIgnoreCase)
+                        castBlock = new IElement[] { dlBlock.First().Children.Last() };
                 }
             }
 
